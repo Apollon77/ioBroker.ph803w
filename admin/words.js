dc74f196341fade9650bf1f5898baffba9a583ff
ioBroker Adapter Creator <noreply@iobroker.net>
2021-05-25 23:03:19 +0200

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,29 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'ph803w adapter settings': {
+        'en': 'Adapter settings for ph803w',
+        'de': 'Adaptereinstellungen für ph803w',
+        'ru': 'Настройки адаптера для ph803w',
+        'pt': 'Configurações do adaptador para ph803w',
+        'nl': 'Adapterinstellingen voor ph803w',
+        'fr': "Paramètres d'adaptateur pour ph803w",
+        'it': "Impostazioni dell'adattatore per ph803w",
+        'es': 'Ajustes del adaptador para ph803w',
+        'pl': 'Ustawienia adaptera dla ph803w',
+        'zh-cn': 'ph803w的适配器设置'
+    },
+    'polling_interval': {
+        'en': 'Data polling interval (s)',
+        'de': 'Datenabfrageintervall (e)',
+        'ru': 'Интервал опроса данных',
+        'pt': 'Intervalo (s) de pesquisa de dados',
+        'nl': 'Interval (en) voor gegevensopvraag',
+        'fr': "Intervalle (s) d'interrogation des données",
+        'it': 'Intervallo (i) di polling dei dati',
+        'es': 'Intervalo (s) de sondeo de datos',
+        'pl': 'Odstęp (y) odpytywania danych',
+        'zh-cn': '数据轮询间隔（秒）'
+    }
+};
